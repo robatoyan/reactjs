98c3aec921c8c0fd0ec1172c0059977d241fbb6b
robatoyan <142260538+robatoyan@users.noreply.github.com>
2023-10-24 20:54:52 +0400

    name

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
-
 <template>
   <h2>GitHub</h2>
+  <h3>vardan</h3>
 </template>
 
 <style></style>
