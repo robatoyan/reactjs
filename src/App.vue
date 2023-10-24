4a7b9a12fa714d1ca5581bf1fbf6a8c3cca27502
robatoyan <142260538+robatoyan@users.noreply.github.com>
2023-10-24 20:53:51 +0400

    project commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <h2>GitHub</h2>
+</template>
+
+<style></style>
